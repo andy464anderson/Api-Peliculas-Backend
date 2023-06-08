0bf18408e715b7d7d2556fcfb725b15709248cf2
andy464anderson <130997796+andy464anderson@users.noreply.github.com>
2023-06-08 12:11:47 +0200

    Add files via upload

--- connection.py
+++ connection.py
@@ -5,10 +5,10 @@ import psycopg2
 def conectar():
     #creamos la conexion a la base de datos
     conn = psycopg2.connect(
-        host="localhost",
-        database="Usuarios",
-        user="postgres",
-        password="4646"
+        host="ec2-18-203-205-71.eu-west-1.compute.amazonaws.com",
+        database="d4lap1mkcchg4o",
+        user="wxdhivoapomzse",
+        password="86fddfec4d5fa2cc95187612d100458298d5b2f6334d00224ca3222f9f17ed02"
     )
     #devolvemos la conexion
     return conn
